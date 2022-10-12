6e43d3565d1eea7cc32a5d1863545561c12fcfa9
Fedbca123 <omarshalaby911@knights.ucf.edu>
2022-10-12 00:10:11 -0400

    removing token

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// A = 123 B = 456 DISCORD_TOKEN =MTAyOTU3MjU4MzM3MDI2ODc3Mg.G_Ov1Q.Q2Ir8rzTefrfX9Kxj6Xys6Lb1aBq7D7UqoE5Lo node index.js https://discordjs.guide/creating-your-bot/#using-environment-variables
-
 // Require the necessary discord.js classes
 const { Client, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
