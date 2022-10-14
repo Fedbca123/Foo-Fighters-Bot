774d3802f87aa42629cde2603615347c8190969d
Fedbca123 <omarshalaby911@knights.ucf.edu>
2022-10-14 00:21:33 -0400

    made commands a separate folder

---
+++ commands/one-piece.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('one-piece')
+		.setDescription('THE ONE PIECE IS REAAAAAL'),
+	async execute(interaction) {
+		await interaction.reply(
+			'https://www.cbr.com/jojos-bizarre-adventure-best-anime-of-all-time/',
+		);
+	},
+};
