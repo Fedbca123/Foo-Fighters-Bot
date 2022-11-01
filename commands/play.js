6482a974e548959204423d1b85a88e9976c9da4f
Fedbca123 <omarshalaby911@knights.ucf.edu>
2022-10-31 22:57:22 -0400

    created queue.js

--- commands/play.js
+++ commands/play.js
@@ -1,36 +1,117 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } =  require('discord.js');
-const { QueryType } = require("discord-player");
+const { MessageEmbed } = require('discord.js');
+const { QueryType } = require('discord-player');
+// const { run } = require('node:test');
 
 module.exports = {
-    data: new SlashCommandBuilder().setName("play")
-        .setDescription("laods songs from youtube")
-        .addSubcommand((subcommand) => {
-            subcommand.setName("song")
-                .setDescription("Loads a single song from a url")
-                .addStringOption((option) => option.setName("url").setDescription("the song's url").setRequired(true))
-        })
-        .addSubcommand((subcommand) => {
-            subcommand
-                .setName("playlist")
-                .setDescription("Loads a playlist of songs from a url")
-                .addStringOption((option) => option.setName("url").setDescription("the playlist's url").setRequired(true))
-        })
-        .addSubcommand((subcommand) => {
-            subcommand
-            .setName("search")
-            .setDescription("Searches for song based on provided keywords")
-            .addStringOption((option) =>
-                option.setName("searchterms").setDescription("the keywords to search").setRequired(true)
-            )
-        })
-        run: async({ client, interaction }) => {
-            if(interaction.member.voice.channel){
-                return interaction.editReply("You need to be in a voice channel to use this command")
-            }
+	data: new SlashCommandBuilder()
+		.setName('play')
+		.setDescription('loads songs from youtube')
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName('song')
+				.setDescription('Loads a single song from a url')
+				.addStringOption((option) =>
+					option
+						.setName('url')
+						// eslint-disable-next-line quotes
+						.setDescription("the song's url")
+						.setRequired(true),
+				),
+		)
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName('playlist')
+				.setDescription('Loads a playlist of songs from a url')
+				.addStringOption((option) =>
+					option
+						.setName('url')
+						// eslint-disable-next-line quotes
+						.setDescription("the playlist's url")
+						.setRequired(true),
+				),
+		)
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName('search')
+				.setDescription('Searches for song based on provided keywords')
+				.addStringOption((option) =>
+					option
+						.setName('searchterms')
+						.setDescription('the keywords to search')
+						.setRequired(true),
+				),
+		),
+	run: async ({ client, interaction }) => {
+		if (!interaction.member.voice.channel) {
+			return interaction.editReply(
+				'You need to be in a VC to run this command',
+			);
+		}
 
-            const queue = await client.player.createQueue(interaction.guild);
+		const queue = await client.player.createQueue(interaction.guild);
+		if (!queue.connection)
+			await queue.connect(interaction.member.voice.channel);
 
-            if(!queue.connection) await queue.connect(interaction.member.voice.channel);
-        }
-}
+		const embed = new MessageEmbed();
+
+		if (interaction.options.getSubcommand() === 'song') {
+			const url = interaction.options.getString('url');
+			const result = await client.player.search(url, {
+				requestedBy: interaction.user,
+				searchEngine: QueryType.YOUTUBE_VIDEO,
+			});
+			if (result.tracks.length === 0) {
+				return interaction.editReply('No Results');
+			}
+			const song = result.tracks[0];
+			await queue.addTrack(song);
+			embed
+				.setDescription(
+					`**[${song.title}](${song.url})** has been added to the Queue`,
+				)
+				.setThumbnail(song.thumbnail)
+				.setFooter({ text: `Duration: ${song.duration}` });
+		} else if (interaction.options.getSubcommand() === 'playlist') {
+			let url = interaction.options.getString('url');
+			const result = await client.player.search(url, {
+				requestedBy: interaction.user,
+				searchEngine: QueryType.YOUTUBE_PLAYLIST,
+			});
+			if (result.tracks.length === 0) {
+				return interaction.editReply('No Results');
+			}
+
+			const playlist = result.playlist;
+			await queue.addTrack(song);
+			embed
+				.setDescription(
+					`**${result.tracks.length} songs from [${playlist.title}](${playlist.url}** has been added to the Queue`,
+				)
+				.setThumbnail(song.thumbnail)
+				.setFooter({ text: `Duration: ${song.duration}` });
+		} else if (interaction.options.getSubcommand() === 'search') {
+			let url = interaction.options.getString('searchterms');
+			const result = await client.player.search(url, {
+				requestedBy: interaction.user,
+				searchEngine: QueryType.AUTO,
+			});
+
+			if (result.tracks.length === 0) {
+				return interaction.editReply('No Results');
+			}
+			const playlist = result.playlist;
+			await queue.addTrack(song);
+			embed
+				.setDescription(
+					`**${result.tracks.length} songs from [${playlist.title}](${playlist.url}** has been added to the Queue`,
+				)
+				.setThumbnail(song.thumbnail)
+				.setFooter({ text: `Duration: ${song.duration}` });
+		}
+		if (!queue.playing) await queue.play();
+		await interaction.editReply({
+			embeds: [embed],
+		});
+	},
+};
